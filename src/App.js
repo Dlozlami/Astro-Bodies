808d95ff38965bcc9e7e3e39851ab901b70332e7
Dlozi Mthethwa <dlozi.mthethwa@gmail.com>
2023-08-18 11:29:42 +0200

    no w3.css

--- src/App.js
+++ src/App.js
@@ -13,33 +13,45 @@ function App() {
     setDOB(event.target.value);
   };
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Handle form submission logic here
+    console.log("Form submitted:", fullName, dob);
+  };
+
   return (
     <div className="app-container">
       <div className="panel">
         <h1>Welcome to AstroBodies!</h1>
         <p>Lets start with your personal details to draw your chart.</p>
 
-        <div className="input-group">
-          <label htmlFor="fullName">Full Name</label>
-          <input
-            type="text"
-            id="fullName"
-            value={fullName}
-            onChange={handleFullNameChange}
-            style={{ padding: "10px" }}
-          />
-        </div>
+        <form onSubmit={handleSubmit} className="form">
+          <div className="input-group">
+            <label htmlFor="fullName">Full Name</label>
+            <input
+              type="text"
+              id="fullName"
+              value={fullName}
+              onChange={handleFullNameChange}
+              style={{ padding: "10px" }}
+            />
+          </div>
 
-        <div className="input-group">
-          <label htmlFor="dob">Date of Birth</label>
-          <input
-            type="date"
-            id="dob"
-            value={dob}
-            onChange={handleDOBChange}
-            style={{ padding: "10px" }}
-          />
-        </div>
+          <div className="input-group">
+            <label htmlFor="dob">Date of Birth</label>
+            <input
+              type="date"
+              id="dob"
+              value={dob}
+              onChange={handleDOBChange}
+              style={{ padding: "10px" }}
+            />
+          </div>
+
+          <button type="submit" className="submit-button">
+            Submit
+          </button>
+        </form>
       </div>
     </div>
   );
